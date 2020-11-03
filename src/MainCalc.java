906fdc21ffa6bb59d74e76a5b359e5316b11cf34
TadasNa <tadas.naudziunas@gmail.com>
2020-11-04 00:26:41 +0200

    I managed to create a calculator by looking at other calculators. Not very proud of it.

---
+++ src/MainCalc.java
@@ -0,0 +1,15 @@
+public class MainCalc {
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println(calc.calculate("5+5"));
+        System.out.println(calc.calculate("5-5"));
+        System.out.println(calc.calculate("5*5"));
+        System.out.println(calc.calculate("5/5"));
+        System.out.println(calc.calculate("5%5"));
+        System.out.println(calc.calculate("5/0"));
+        System.out.println(calc.calculate("5!"));
+        System.out.println(calc.calculate("5*1+2-4!"));
+        System.out.println(calc.calculate("5.57*1+2-4!"));
+    }
+}
